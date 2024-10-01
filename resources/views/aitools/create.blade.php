27c290a39d4223257b96010f6d945d3a326d45f4
nzsolt0601 <nagy.zsolt2005@tanulo.boronkay.hu>
2024-10-01 19:22:56 +0200

    Laravel_NZS_10_01

---
+++ resources/views/aitools/create.blade.php
@@ -0,0 +1,54 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Új AI Eszköz</h1>
+
+@error('name')
+<div class="alert alert-warning">{{$message}}</div>
+@enderror
+
+<form action="{{ route('aitools.store') }}" method="post">
+    @csrf
+    <fieldset>
+        <label for="name">Az AI eszköz neve</label>
+        <input type="text" name="name" id="name">
+    </fieldset>
+    <fieldset>
+        <label for="category_id">Kategória</label>
+        <select name="category_id" id="category_id">
+            @foreach ($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+        </select>
+    </fieldset>
+    <fieldset>
+        <label for="description">Leírás</label>
+        <textarea name="description" id="description"></textarea>
+    </fieldset>
+    <fieldset>
+        <label for="link">Link</label>
+        <input type="text" name="link" id="link">
+    </fieldset>
+    <fieldset>
+        <label for="hasFreePlan">Van ingyenes változat?</label>
+        <input type="checkbox" name="hasFreePlan" id="hasFreePlan">
+    </fieldset>
+    <fieldset>
+        <label for="price">Ár (havonta Ft-ban)</label>
+        <input type="number" name="price" id="price">
+    </fieldset>
+
+    <fieldset>
+        <label for="tags">Címkék</label>
+        <select name="tags[]" id="tags" multiple>
+            @foreach ($tags as $tag)
+            <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+            @endforeach
+        </select>
+    </fieldset>
+    
+    <button type="submit">Ment</button>
+</form>
+
+@endsection
